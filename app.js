5b8e4053d2f207f4da50798c3519a40b46d5f847
chandanch <chandan.cec107@gmail.com>
2024-01-14 20:04:48 +0530

    initial commit ts compiler :tada:

---
+++ app.js
@@ -0,0 +1,5 @@
+var samer = {
+    name: 'Sammer Buneos',
+    age: 123,
+};
+console.log('User Details', samer);
---
+++ app.ts
@@ -0,0 +1,11 @@
+type User = {
+	name: string;
+	age: number;
+};
+
+const samer: User = {
+	name: 'Sammer Buneos',
+	age: 123,
+};
+
+console.log('User Details', samer);
